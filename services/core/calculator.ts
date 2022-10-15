f7ede60a011ac9097edfbc2aeabb1322c100c091
Soenke Ruempler <soenke@ruempler.eu>
2022-10-15 20:36:32 +0200

    feat: provide available rates for a rate

--- services/core/calculator.ts
+++ services/core/calculator.ts
@@ -1,3 +1,5 @@
+import {expect} from "vitest";
+
 export function getMostInexpensiveRate(weight_g: number, length_cm: number, width_cm: number, height_cm: number): string {
 
   if (weight_g > 31500) {
@@ -21,4 +23,127 @@ export function getMostInexpensiveRate(weight_g: number, length_cm: number, widt
     return '2 kg - Päckchen M'
   }
   return '2 kg - Päckchen S';
+}
+
+interface ShippingRate {
+  rate: string,
+  max: {
+    weight_g: number,
+    length_cm: number,
+    width_cm: number,
+    height_cm: number,
+  },
+  liability_eur: null|number,
+  tracking: boolean
+}
+
+export const paeckchen_s = {
+  rate: '2 kg - Päckchen S',
+  max: {
+    weight_g: 2000,
+    length_cm: 35,
+    width_cm: 25,
+    height_cm: 10,
+  },
+  liability_eur: null,
+  tracking: false,
+};
+export const paeckchen_m = {
+  rate: '5 kg - Päckchen M',
+  max: {
+    weight_g: 5000,
+    length_cm: 60,
+    width_cm: 30,
+    height_cm: 15,
+  },
+  liability_eur: null,
+  tracking: false,
+};
+export const paket_2_kg = {
+  rate: '2 kg - Paket',
+  max: {
+    weight_g: 2000,
+    length_cm: 60,
+    width_cm: 30,
+    height_cm: 15,
+  },
+  liability_eur: 500,
+  tracking: false,
+};
+export const paket_5_kg = {
+  rate: '5 kg - Paket',
+  max: {
+    weight_g: 5000,
+    length_cm: 120,
+    width_cm: 60,
+    height_cm: 60,
+  },
+  liability_eur: 500,
+  tracking: true,
+};
+export const paket_10_kg = {
+  rate: '10 kg - Paket',
+  max: {
+    weight_g: 10000,
+    length_cm: 120,
+    width_cm: 60,
+    height_cm: 60,
+  },
+  liability_eur: 500,
+  tracking: true,
+};
+export const packet_31_5_kg = {
+  rate: '31.5 kg - Paket',
+  max: {
+    weight_g: 31500,
+    length_cm: 120,
+    width_cm: 60,
+    height_cm: 60,
+  },
+  liability_eur: 500,
+  tracking: true,
+};
+
+export function getPossibleRates(rate: string): ShippingRate[] {
+  switch (rate) {
+    case '2 kg - Päckchen M':
+      return [
+        paeckchen_m,
+        paket_2_kg,
+        paket_5_kg,
+        paket_10_kg,
+        packet_31_5_kg,
+      ];
+    case '2 kg - Paket':
+      return [
+        paket_2_kg,
+        paket_5_kg,
+        paket_10_kg,
+        packet_31_5_kg,
+      ];
+    case '5 kg - Paket':
+      return [
+        paket_5_kg,
+        paket_10_kg,
+        packet_31_5_kg,
+      ];
+    case '10 kg - Paket':
+      return [
+        paket_10_kg,
+        packet_31_5_kg,
+      ];
+    case '31.5 kg - Paket':
+      return [
+        packet_31_5_kg,
+      ];
+    default:
+      return [
+        paeckchen_s,
+        paeckchen_m,
+        paket_2_kg,
+        paket_5_kg,
+        paket_10_kg,
+        packet_31_5_kg,
+      ];
+  }
 }
--- services/test/calculator.test.js
+++ services/test/calculator.test.js
@@ -1,5 +1,10 @@
 import { describe, it, expect } from "vitest";
-import { getMostInexpensiveRate } from "@shipping-price-calculator/core/calculator"
+import {
+    getMostInexpensiveRate, getPossibleRates, paeckchen_s, paeckchen_m, paket_2_kg,
+    paket_5_kg,
+    paket_10_kg,
+    packet_31_5_kg,
+} from "@shipping-price-calculator/core/calculator"
 describe("shipping calculator", () => {
   it("should calculate", () => {
 
@@ -26,5 +31,54 @@ describe("shipping calculator", () => {
     expect(() => { getMostInexpensiveRate(31500, 121, 60, 60)}).toThrow(RangeError);
     expect(() => { getMostInexpensiveRate(31500, 120, 61, 60)}).toThrow(RangeError);
     expect(() => { getMostInexpensiveRate(31500, 120, 60, 61)}).toThrow(RangeError);
+  });
+
+  it("should get possible rates and options", () => {
+
+      expect(getPossibleRates('2 kg - Päckchen S')).toEqual(
+        [
+          paeckchen_s,
+          paeckchen_m,
+          paket_2_kg,
+          paket_5_kg,
+          paket_10_kg,
+          packet_31_5_kg,
+        ]
+    );
+      expect(getPossibleRates('2 kg - Päckchen M')).toEqual(
+          [
+              paeckchen_m,
+              paket_2_kg,
+              paket_5_kg,
+              paket_10_kg,
+              packet_31_5_kg,
+          ]
+      );
+      expect(getPossibleRates('2 kg - Paket')).toEqual(
+          [
+              paket_2_kg,
+              paket_5_kg,
+              paket_10_kg,
+              packet_31_5_kg,
+          ]
+      );
+      expect(getPossibleRates('5 kg - Paket')).toEqual(
+          [
+              paket_5_kg,
+              paket_10_kg,
+              packet_31_5_kg,
+          ]
+      );
+      expect(getPossibleRates('10 kg - Paket')).toEqual(
+          [
+              paket_10_kg,
+              packet_31_5_kg,
+          ]
+      );
+      expect(getPossibleRates('31.5 kg - Paket')).toEqual(
+          [
+              packet_31_5_kg,
+          ]
+      );
   });
 });
